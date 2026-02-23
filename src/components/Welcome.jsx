018f70a2e02af1e41e6ca2cc1978a9734f53f827
alvaromassingue <alvaroruimassingue658@gmail.com>
2026-02-23 02:23:09 +0200

    pronto para continuar

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Welcome = () => {
   const description =
-    "Sistema de visualização dos resultados dos exames de admissão";
+    "Sistema de Visualização dos Resultados dos Exames de Admissão";
 
   const [index, setIndex] = useState(0);
   const [showDescription, setShowDescription] = useState("");
@@ -18,9 +18,9 @@ const Welcome = () => {
   }, [index]);
 
   return (
-    <div className="w-full md:h-100 h-95 p-5 m-0 mt-10 md:mt-20  flex items-center flex-wrap wrap-break-word bg-linear-to-r from-sky-50 via-sky-200 to-sky-50">
+    <div className="w-full md:h-100 h-95  m-0 mt-10 md:mt-20  flex items-center flex-wrap wrap-break-word bg-linear-to-r from-sky-50 via-sky-200 to-sky-50">
       <div className=" basis-full grow shrink ">
-        <div className="h-3/4 flex justify-center items-center text-center">
+        <div className="h-3/4 flex justify-center items-center text-center w-full">
           <h1 className="text-3xl font-black bg-linear-to-r from-sky-400 via-sky-500 to-blue-700 bg-clip-text text-transparent md:text-5xl">
             {showDescription}
           </h1>
