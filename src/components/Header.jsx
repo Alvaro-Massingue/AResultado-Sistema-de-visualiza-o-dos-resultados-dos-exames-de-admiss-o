3e0f5a0d83ac100fa9e95ba74c33c26c208e41f9
alvaromassingue <alvaroruimassingue658@gmail.com>
2026-03-06 19:38:12 +0200

    Actualiza a UI adicionando fundo nos botoes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,41 +12,41 @@ const Header = (props) => {
   return (
     <div
       className={
-        "fixed top-0 left-0 z-10 flex w-full items-center h-18 md:h-23 flex-wrap bg-sky-500 shadow-sm  shadow-gray-500 pr-5"
+        "fixed top-0 left-0 z-10  flex w-full items-center h-18 md:h-23 flex-wrap bg-sky-500 shadow-sm  shadow-gray-500 "
       }
     >
-      <div className="flex items-center w-1/2 ">
+      <div className="flex items-center  w-1/2 h-full">
         <img src={logo} className="w-15 md:w-20" />
       </div>
-      <div className="w-1/2 text-right">
+      <div className="w-1/2 text-right h-full flex items-center justify-end pr-5">
         {props.isAuthenticated ? (
-          <div className="flex justify-end">
+          <div  onClick={() => navigate("/")} className="flex justify-end hover:cursor-pointer hover:border-2 hover:border-cyan-400 bg-white rounded-2xl p-3 gap-2 grow-0 min-w-[14%]">
             <span
               onClick={() => navigate("/")}
-              className="material-symbols-outlined  text-white cursor-pointer
+              className="material-symbols-outlined  text-sky-500 cursor-pointer
           [font-variation-settings:'FILL'_1,'wght'_400,'GRAD'_0,'opsz'_24]"
             >
               logout
             </span>
             <button
               onClick={() => navigate("/")}
-              className="text-white cursor-pointer"
+              className="text-sky-500 cursor-pointer"
             >
               Sair
             </button>
           </div>
         ) : (
-          <div className="flex justify-end">
+          <div onClick={() => loginPanel()} className="flex hover:cursor-pointer hover:border-2 hover:border-cyan-400 justify-end bg-white rounded-2xl p-3 gap-2 grow-0 min-w-[14%]">
             <span
               onClick={() => loginPanel()}
-              className="material-symbols-outlined  text-white cursor-pointer
+              className="material-symbols-outlined  text-sky-500 cursor-pointer
           [font-variation-settings:'FILL'_1,'wght'_400,'GRAD'_0,'opsz'_24]"
             >
               login
             </span>
             <button
               onClick={() => loginPanel()}
-              className="text-white cursor-pointer"
+              className="text-sky-500 cursor-pointer"
             >
               Login
             </button>
