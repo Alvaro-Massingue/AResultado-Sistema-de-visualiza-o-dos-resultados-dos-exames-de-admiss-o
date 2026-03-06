83e12c98579c39b0c5494e96f18d355f235d559e
alvaromassingue <alvaroruimassingue658@gmail.com>
2026-03-06 18:30:36 +0200

    Remove a borda no bottom

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -25,7 +25,7 @@ const SearchResults = (props) => {
   const [selectPeriods, setSelectPeriods] = useState(false);
 
   return (
-    <div className="w-full flex border-b-2 border-gray-300 flex-wrap gap-2 min-h-45 md:items-start justify-center items-center pt-3 pb-5">
+    <div className="w-full flex flex-wrap gap-2 min-h-45 md:items-start justify-center items-center pt-3 pb-5">
       <label
         className=" text-gray-600 basis-70 shrink grow-0 md:basis-1"
         htmlFor="course"
@@ -38,7 +38,7 @@ const SearchResults = (props) => {
             Preencha este campo
           </p>
         ) : (
-          ""  
+          ""
         )}
         <select
           id="course"
