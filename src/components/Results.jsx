4af90c39ecdf74fe272bc07089988defa0aa895d
alvaromassingue <alvaroruimassingue658@gmail.com>
2026-03-06 18:44:08 +0200

    Actualiza o fundoo

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,10 @@
 const Results = (props) => {
   return (
-    <div className="flex flex-col items-center shadow-lg shadow-black pt-6 pb-8 gap-10 bg-linear-to-t from-sky-50 via-gray-50 to-white">
+    <div className="flex flex-col items-center shadow-lg shadow-black pt-6 pb-8 gap-10 bg-white">
       {props.results.map((result) => (
         <div
           key={result.id}
-          className="bg-white rounded-2xl shadow-lg border border-sky-500 shadow-gray-300 font-extralight w-[90%]  p-2"
+          className="bg-white rounded-2xl  border border-sky-500  font-extralight w-[90%]  p-2"
         >
           <div className="flex gap-2">
             <p className="text-gray-700 ">Curso:</p>
