bf428d00be3189c067f8817073e49ab49a28dce0
Alvaro Rui Massingue <alvaroruimassingue25@gmail.com>
2026-04-07 21:29:08 +0200

    Subistitui a imagem do fundo pelo fundo gradiente

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -15,10 +15,15 @@ const Welcome = () => {
       }, 60);
       return () => clearTimeout(descriptionText);
     }
-  }, [index,description]);
+  }, [index, description]);
 
   return (
-    <div className="w-full md:h-100 h-95  m-0 mt-10 md:mt-20  flex items-center flex-wrap wrap-break-word bg-[url('./assets/background.jpg')] bg-center bg-fixed bg-contain">
+    <div
+      className="w-full md:h-100 h-95  m-0 mt-10 md:mt-20  flex items-center flex-wrap wrap-break-word"
+      style={{
+        background: `repeating-linear-gradient(to right,transparent 0px,transparent 45px,rgba(0, 128, 255, 0.100) 45px,rgba(0, 128, 255, 0.100) 46px),repeating-linear-gradient(to bottom,transparent 0px,transparent 45px,rgba(0, 128, 255, 0.100)  45px,rgba(0, 128, 255, 0.100) 46px
+)`}}
+      >
       <div className=" basis-full grow shrink flex justify-center items-center flex-wrap gap-10">
         <div className="h-3/4 flex justify-center items-center text-center md:w-[70%] w-full ">
           <h1 className="text-3xl font-black bg-linear-to-t from-sky-400 via-sky-500 to-blue-700 bg-clip-text text-transparent md:text-5xl">
