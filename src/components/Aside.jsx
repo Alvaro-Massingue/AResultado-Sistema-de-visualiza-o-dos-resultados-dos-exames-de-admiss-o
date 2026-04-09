ed315a2b670f18250ecda0efa67042755a9f7e74
Alvaro Rui Massingue <alvaroruimassingue25@gmail.com>
2026-04-09 13:31:08 +0200

    Actualizacao geral do sistema

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -11,21 +11,21 @@ const Aside = ({
 }) => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center w-[25%] h-full shadow-lg shadow-gray-600 fixed">
-      <div className="h-[10%] w-full pl-4 pt-5 mb-10 shadow-lg flex gap-3">
+    <div className="flex flex-col items-center justify-center w-[25%] h-full bg-white fixed">
+      <div className="h-[10%] w-full pl-4 pt-5 mb-10 flex gap-2">
         <span
           className="material-symbols-outlined text-sky-500 
-          [font-variation-settings:'FILL'_1,'wght'_800,'GRAD'_0,'opsz'_24]"
+          [font-variation-settings:'FILL'_1,'wght'_200,'GRAD'_0,'opsz'_24]"
         >
           person
         </span>
-        <p className="text-sky-500">{userAdd.name}</p>
+        <p className="text-sky-500">{userAdd.name}istrador</p>
       </div>
       <nav className="h-[90%] flex flex-col gap-4 w-[90%] items-start text-gray-900">
         <div className="flex items-center w-full p-4 text-start shadow-2xs gap-2">
           <span
             className="material-symbols-outlined text-sky-500 
-          [font-variation-settings:'FILL'_1,'wght'_400,'GRAD'_0,'opsz'_24]"
+          [font-variation-settings:'FILL'_1,'wght'_200,'GRAD'_0,'opsz'_24]"
           >
             dashboard
           </span>
@@ -47,7 +47,7 @@ const Aside = ({
         <div className="flex items-center w-full p-4 text-start shadow-2xs gap-2">
           <span
             className="material-symbols-outlined text-sky-500 
-          [font-variation-settings:'FILL'_1,'wght'_400,'GRAD'_0,'opsz'_24]"
+          [font-variation-settings:'FILL'_1,'wght'_200,'GRAD'_0,'opsz'_24]"
           >
             edit
           </span>
@@ -69,7 +69,7 @@ const Aside = ({
         <div className="flex items-center w-full p-4 text-start shadow-2xs gap-2">
           <span
             className="material-symbols-outlined text-sky-500 
-          [font-variation-settings:'FILL'_1,'wght'_400,'GRAD'_0,'opsz'_24]"
+          [font-variation-settings:'FILL'_1,'wght'_200,'GRAD'_0,'opsz'_24]"
           >
             add_circle
           </span>
@@ -85,7 +85,7 @@ const Aside = ({
                 : "cursor-pointer  hover:text-sky-500 focus:text-sky-500"
             }
           >
-            Adicionar novos resultados
+            Adicionar resultados
           </button>
         </div>
       </nav>
--- src/components/ManageResults.jsx
+++ src/components/ManageResults.jsx
@@ -1,6 +1,6 @@
 const ManageResults = () => {
   return (
-    <div className="w-3/4 flex flex-col gap-4 absolute right-0 items-center justify-center h-full">
+    <div className="w-3/4 flex flex-col bg-gray-50 gap-4 absolute right-0 items-center justify-center h-full">
       <h1 className="text-6xl text-sky-500">Gerir resultados</h1>
       <p className="text-gray-600">Pagina em desenvolvimento</p>
     </div>
