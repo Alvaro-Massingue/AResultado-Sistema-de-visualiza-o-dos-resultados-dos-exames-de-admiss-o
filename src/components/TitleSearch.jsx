49d02ff3a1f4d7d8570a967f437574e6a8718d90
Alvaro Rui Massingue <alvaroruimassingue25@gmail.com>
2026-04-11 22:01:26 +0200

    Atualiza o home com foto de fundo

--- src/components/TitleSearch.jsx
+++ src/components/TitleSearch.jsx
@@ -1,7 +1,7 @@
 const TitleSearch = () => {
   return (
-    <div className="bg-white w-full min-h-35 md:min-h-60 flex flex-wrap items-center justify-center">
-      <h1 className="bg-gradient-to-t from-blue-600 via-sky-500 to-blue-700 bg-clip-text text-transparent font-black text-2xl md:text-3xl basis-70 shrink grow text-center">
+    <div className="bg-[url('/src/assets/ujc.png')] bg-cover bg-center w-full min-h-35 md:min-h-60 flex flex-wrap items-center justify-center">
+      <h1 className="text-white font-black text-2xl md:text-3xl basis-70 shrink grow text-center">
         BUSCAR RESULTADOS
       </h1>
     </div>
