d6d9130884e529cda793c182031b87980a4774c1
alvaromassingue <alvaroruimassingue658@gmail.com>
2026-04-11 23:31:06 +0300

    Atualiza o sistema

--- src/components/TitleSearch.jsx
+++ src/components/TitleSearch.jsx
@@ -1,6 +1,6 @@
 const TitleSearch = () => {
   return (
-    <div className="bg-[url('/src/assets/ujc.png')] bg-cover bg-center w-full min-h-35 md:min-h-60 flex flex-wrap items-center justify-center">
+    <div className="bg-[url('/src/assets/ujc.jpg')] bg-cover bg-center w-full min-h-35 md:min-h-60 flex flex-wrap items-center justify-center">
       <h1 className="text-white font-black text-2xl md:text-3xl basis-70 shrink grow text-center">
         BUSCAR RESULTADOS
       </h1>
